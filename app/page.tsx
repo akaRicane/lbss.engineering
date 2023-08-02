1518fd45a950bc67a3c27a2614a47024b27c9767
akaRicane <ricane@lbss.art>
2023-08-03 01:15:45 +0200

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div>Welcome to lbss.engineering!</div>
+      <div>work in progress</div>
       <Link href="/about">go to about</Link>
     </main>
   );
