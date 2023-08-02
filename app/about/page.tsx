6f4b62371bc26d8b87e71766fa7599474137a899
akaRicane <ricane@lbss.art>
2023-08-03 01:37:21 +0200

    added lbss.art website as link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,9 @@ import Link from "next/link";
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>About lbss.engineering!</div>
+      <div>
+        visit <Link href="http://www.lbss.art">lbss.art website</Link>
+      </div>
       <Link href="/">go to home</Link>
     </main>
   );
