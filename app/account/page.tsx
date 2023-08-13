916d00d7ccc83d2ab916de9cefdf6adc1153ce6e
akaRicane <ricane@lbss.art>
2023-08-13 05:08:59 +0200

    testing /ping with credentials

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,10 +2,30 @@
 
 import { useEffect } from "react";
 import { useCoreContext } from "../contexts/CoreContext";
+import EnvGetter from "../components/EnvGetter";
 import "../styles/page.products.css";
 
 const Account = () => {
   const { tokenId } = useCoreContext();
+
+  useEffect(() => {
+    const apiUrl = EnvGetter("ENGINE_API_URL");
+    if (tokenId) {
+      console.log(`${apiUrl}/ping`, tokenId);
+      fetch(`${apiUrl}/ping`, {
+        method: "GET",
+        headers: {
+          "lbss-cloud-auth-token": tokenId,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    } else {
+      console.log("no tokenid, ping not sent");
+    }
+  }, [tokenId]);
 
   return (
     <div className="page">
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   env: {
     ENGINE_API_LOGIN_URL_DEV: 'https://lbss-cloud-connect.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4uv2bnr29gnt02idnpd46h9hpf&redirect_uri=http://localhost:3000/callback',
-    ENGINE_API_LOGIN_URL_PROD: 'https://lbss-cloud-connect.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4uv2bnr29gnt02idnpd46h9hpf&redirect_uri=http://lbss.engineering/callback'
+    ENGINE_API_LOGIN_URL_PROD: 'https://lbss-cloud-connect.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4uv2bnr29gnt02idnpd46h9hpf&redirect_uri=http://lbss.engineering/callback',
+    ENGINE_API_URL: 'https://lujgfln4y7.execute-api.eu-west-3.amazonaws.com/dev'
   }
 }
 
