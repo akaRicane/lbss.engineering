ed7b43205a4adeee7a5b84c93e54553964e865b7
akaRicane <ricane@lbss.art>
2023-08-20 19:37:09 +0200

    init 0.2.7

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    APP_VERSION: "0.2.6",
+    APP_VERSION: "0.2.7",
     ENGINE_API_LOGIN_URL_DEV: 'https://lbss-cloud-connect.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4uv2bnr29gnt02idnpd46h9hpf&redirect_uri=http://localhost:3000/callback',
     ENGINE_API_LOGIN_URL_PROD: 'https://lbss-cloud-connect.auth.eu-west-3.amazoncognito.com/login?response_type=token&client_id=4uv2bnr29gnt02idnpd46h9hpf&redirect_uri=https://lbss.engineering/callback',
     ENGINE_API_URL: 'https://lujgfln4y7.execute-api.eu-west-3.amazonaws.com/dev',
